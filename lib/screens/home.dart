7e515e74be0e97cf70519335f6f69b89b4352f19
MaryneZa <maryne6916@gmail.com>
2025-08-18 22:57:01 +0700

    try column widget

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,14 +5,38 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color.fromARGB(255, 207, 188, 131),
-      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
-      padding: EdgeInsets.all(20),
-      child: Text(
-        "I'm a child!",
-        style: TextStyle(fontSize: 30, letterSpacing: 10),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          color: const Color.fromARGB(255, 207, 188, 131),
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          padding: EdgeInsets.all(20),
+          child: Text(
+            "I'm a child!",
+            style: TextStyle(fontSize: 30, letterSpacing: 10),
+          ),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 207, 188, 131),
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          padding: EdgeInsets.all(20),
+          child: Text(
+            "I'm a child!",
+            style: TextStyle(fontSize: 30, letterSpacing: 10),
+          ),
+        ),
+        Container(
+          color: const Color.fromARGB(255, 207, 188, 131),
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          padding: EdgeInsets.all(20),
+          child: Text(
+            "I'm a child!",
+            style: TextStyle(fontSize: 30, letterSpacing: 10),
+          ),
+        ),
+      ],
     );
   }
 }
